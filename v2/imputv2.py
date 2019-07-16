e5f587bc929bdb450c3b692b378e741431d421ac
JackKiefer <jack.c.kiefer@gmail.com>
2019-07-16 13:03:46 -0600

    First attempt at manual imputation. Sucks.

--- v2/imputv2.py
+++ v2/imputv2.py
@@ -4,6 +4,9 @@ from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm
 from scipy import stats
 import pandas as pd
+import numpy as np
+from hyperopt import fmin, tpe, hp
+import os
 
 """
 Algorithm
@@ -35,23 +38,94 @@ CCs = [ 'CC_PolPop',
         'CC_Writing',
         'CC_Texts',
         'CC_Money']
- 
 
-def buildModel(seshat):
-#    for cc in CCs:
+# Really, REALLY janky way of going about parsing p-values from the
+# regression result. But hey, it works. Returns a list of significant coeffs
+def parseResults(fit):
+    # Split the summary string into lines
+    lines = fit.summary().as_csv().split('\n')[10:-7]
+    # Write to a temporary file to create a proper csv
+    with open('temp','w') as f:
+        for line in lines:
+            f.write(line + '\n')
+    # Now, read that csv into a dataframe and select the P values
+    res = pd.read_csv('temp',index_col=0)
+    pvals = res['P>|t| ']
+    # Clean up the temp ile
+    os.remove('temp')
+    # Turn it into a pretty list of significant coefficients with initial guesses
+    return [pred.replace(' ','') for pred, val in pvals.items() if val < 0.05]
+
+
+# Use a least-squares regression to select an appropriate model by using
+# p-values to determine the most significant terms to include 
+def selectPredictors(seshat):
     predictors = {}
     for cc in CCs:
         target = seshat[cc]
+        # Add all terms but the target term
         X = sm.add_constant(seshat.drop(cc,axis='columns'))
+        # Compute a regression model using least squares
         ordinaryLeastSquares  = sm.OLS(target, X)
-        results               = ordinaryLeastSquares.fit(transform=False)
-        lines = results.summary().as_csv().split('\n')[10:-7]
-        with open('temp','w') as f:
-            for line in lines:
-                f.write(line + '\n')
-        res = pd.read_csv('temp',index_col=0)['P>|t| ']
-        predictors[cc] = [pred.replace(' ','') for pred, val in res.items() if val < 0.05]
-    return seshat
+        fit                   = ordinaryLeastSquares.fit(transform=False)
+        predictors[cc]        = parseResults(fit)
+    return predictors
+
+def p2prediction(predicted, actual):
+    num  = np.sum([(p - a)**2 for p,a in list(zip(predicted,actual))])
+    yBar = np.mean(actual)
+    dem  = np.sum([(yBar - a)**2 for a in actual])
+    return 1.0 - (num/dem)
+
+def regionKFold(seshat):
+    return [
+        (seshat[seshat['Region'] != reg], seshat[seshat['Region'] == reg])
+            for reg in seshat['Region'].unique() 
+    ]
+
+def predict(targetVar, parms, trainSet):
+    return
+
+def evaluateModel(seshat, targetVar, parms):
+    p2s = []
+    # For each slice in the k-fold validation
+    for trainSet,testSet in regionKFold(seshat):
+        # Use the train set to predict the given variable
+        predicted = predict(targetVar, parms, trainSet)
+        actual    = list(testSet[targetVar])
+        # Use the predicted and actual data to get a p2 value
+        p2s.append(p2prediction(predicted, actual))
+    # Return the average of all p2 values
+    return np.mean(p2s)
+
+def makeSearchSpace(targetVar, predictors):
+    allVars = ['constant','polPop','polTerr','capPop','hier','govt','infra',
+               'writing', 'texts', 'money']
+    searchSpace = []
+    for var in allVars:
+        if var != targetVar and var in predictors:
+            searchSpace.append(hp.uniform(var, -5.0, 5.0))
+        else:
+            searchSpace.append(hp.uniform(var, 0.0, 0.0))
+    return tuple(searchSpace)
+
+def optimizeCoeffs(seshat,targetVar,predictors):
+    searchSpace = makeSearchSpace(targetVar, predictors)
+    return fmin(
+        fn=(lambda parms : evaluateModel(seshat, targetVar, parms)),
+        space=searchSpace, algo=tpe.suggest, max_evals=1000
+        )
+
+
+def buildModel(seshat):
+    predictors = selectPredictors(seshat)
+    model = {}
+    # For each predictor
+    for targetVar,predictors in predictors.iteritems():
+        # Find the best coefficients for predicting the variable
+        model[targetVar] = optimizeCoeffs(seshat,targetVar,predictors)
+    # Return all info found
+    return model
 
 # Pair down to only datapoints with all 51 variables
 def pairDown(fullSeshat):
@@ -65,7 +139,7 @@ def impute(fullSeshat):
 
     # Build the full imputation model
     # Use the model to impute the dataset 20 times.
-    seshat = buildModel(seshat)
+    model = buildModel(seshat)
 
     # (Each imputation introduces variance from sampling the residual to add to the final vals)
 
